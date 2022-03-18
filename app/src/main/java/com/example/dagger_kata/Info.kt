9e5195fc156fb238c99e6b27e664936ae75b0851
Liza Baby <lbaby1@ford.com>
2022-03-18 12:40:46 -0400

    Added info class. WIP databinding

---
+++ app/src/main/java/com/example/dagger_kata/Info.kt
@@ -0,0 +1,4 @@
+package com.example.dagger_kata
+
+class Info {
+}
--- app/src/main/java/com/example/dagger_kata/MainActivity.kt
+++ app/src/main/java/com/example/dagger_kata/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var info: Info
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text_view.text = info.text
     }
 }
