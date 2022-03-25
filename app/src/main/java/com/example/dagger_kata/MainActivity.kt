6d2ccd77cef22a2555cdf69f9c19619049d8036a
Dustin Thurston <dthurst4@ford.com>
2022-03-25 12:44:29 -0400

    added injected info class

--- app/src/main/java/com/example/dagger_kata/MainActivity.kt
+++ app/src/main/java/com/example/dagger_kata/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.dagger_kata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
     lateinit var info: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
