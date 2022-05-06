0189e91d40e4854490a99d356ba8c3ea1c578f7d
Ty Austin <tausti20@ford.com>
2022-05-06 12:26:39 -0400

    Create PictureActivity

---
+++ picture/src/main/java/com/tausti20/picture/PictureActivity.kt
@@ -0,0 +1,11 @@
+package com.tausti20.picture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+    }
+}
